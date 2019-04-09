29dc778d923fa3734c310a363c48339ebb4c7bf7
Natasha Giusti <natashagiusti@natashas-mbp.lan>
2019-04-08 22:12:22 -0700

    some time conversion and math stuff happening

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -65,10 +65,21 @@ $(document).ready(function () {
         var train = childSnapshot.val().name;
         var destination = childSnapshot.val().destination;
         var frequency = childSnapshot.val().frequency;
-        
+
         // time variables
         var currentTime = moment();
+        var firstTrainConverted = moment(childSnapshot.val().firstTrain, "HH:mm").subtract(1, "years");
+        var timeDiff = moment().diff(moment(firstTrainConverted), "minutes");
+        var remainder = timeDiff % frequency;
+        
 
+
+
+        // time info
+        console.log("Current time: " + moment().format("HHmm"));
+        console.log(firstTrainConverted);
+        console.log("Difference in time: " + timeDiff);
+        console.log("Time remaining: " + remainder)
         // train info
         console.log(train);
         console.log(destination);
@@ -80,83 +91,13 @@ $(document).ready(function () {
             $("<td>").text(train),
             $("<td>").text(destination),
             $("<td>").text(frequency),
-            $("<td>").text(),
-            $("<td>").text(),
-            );
+            $("<td>").text("next"),
+            $("<td>").text("minutes"),
+        );
 
-            $("#train-table > tbody").append(newRow);
+        $("#train-table > tbody").append(newRow);
     })
 
 
 
 })
-
-
-
-
-// database.ref().on("child_added", function (childSnapshot) {
-//     // console.log(childSnapshot.val());
-
-//     var name = childSnapshot.val().name;
-//     var number = childSnapshot.val().number;
-//     var destination = childSnapshot.val().destination;
-//     var time = childSnapshot.val().time;
-//     var frequency = childSnapshot.val().frequency;
-
-//     // console.log(name, number, destination, time, frequency);
-
-//     //time formatting
-//     //this required a LOT of googling to figure out
-//     var frequency = parseInt(frequency);
-//     var currentTime = moment();
-
-//     //console.log("Current time: " + moment().format("HHmm"));
-
-//     //originally used mil format of HHMM but that failed with a null value
-//     //looked up potential faults and it turns out that moment.js must use
-//     //HH:mm for mil/euro time format
-//     var dateConvert = moment(childSnapshot.val().time, "HHmm").subtract(1, "years");
-
-//     //console.log("DATE CONVERTED: " + dateConvert);
-
-//     var trainTime = moment(dateConvert).format("HHmm");
-
-//     //console.log("Train time : " + trainTime);
-
-//     //difference bw the times
-//     var timeConvert = moment(trainTime, "HHmm").subtract(1, "years");
-//     var timeDifference = moment().diff(moment(timeConvert), "minutes");
-
-//     //console.log("Difference in time: " + timeDifference);
-
-//     //remainder
-//     var timeRemaining = timeDifference % frequency;
-
-//     //console.log("Time remaining: " + timeRemaining);
-
-//     //time until next train
-//     var timeAway = frequency - timeRemaining;
-
-//     //console.log("Minutes until next train: " + timeAway);
-
-//     //next train arrival
-//     var nextArrival = moment().add(timeAway, "minutes");
-
-//     //figured out that adding "A" at the end of HH:mm will add AM or PM
-//     //given that this is mil/euro format, the AM/PM is not necessary
-//     //console.log("Arrival time: " + moment(nextArrival).format("HHmm"));
-
-//     var arrivalDisplay = moment(nextArrival).format("HHmm");
-
-//     //append data to table
-//     $("#boardText").append(
-//         "<tr><td id='nameDisplay'>" + childSnapshot.val().name +
-//         "<td id='numberDisplay'>" + childSnapshot.val().number +
-//         "<td id='destinationDisplay'>" + childSnapshot.val().destination +
-//         "<td id='frequencyDisplay'>" + childSnapshot.val().frequency +
-//         "<td id='arrivalDisplay'>" + arrivalDisplay +
-//         "<td id='awayDisplay'>" + timeAway + " minutes until arrival" + "</td></tr>");
-
-//     // console.log(arrivalDisplay);
-//     // console.log(timeAway);
-// });
